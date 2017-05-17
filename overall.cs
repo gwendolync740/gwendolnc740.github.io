1785aaeab76e77efb774e38399ca0b5e7a4ae25a
gwendolync740 <gwendolync740@gmail.com>
2017-05-17 13:37:43 -0400

    Carnival game

---
+++ overall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overall : MonoBehaviour {
+	public GameObject mover;
+	public GameObject[] enemies;
+	public Renderer rend;
+	Vector3 moverOrigin; 
+
+	// Use this for initialization
+	void Start () {
+		moverOrigin = mover.transform.position;
+		for (int i = 0; i < 18; i++) {
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("game object is at..." + mover.transform.position);
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			mover.transform.position += new Vector3 (-1,0,0);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			mover.transform.position += new Vector3 (1, 0, 0);
+		}
+
+		if ((mover.transform.position.x > 5)
+		    || (mover.transform.position.x < -5)) {
+			mover.transform.position = moverOrigin;
+		}
+		
+	}
+
+	void OnCollisionEnter (Collision balloons){
+		if(balloons.gameObject.tag == "balloon"){
+			Destroy(gameObject);
+			rend.material.color = Color.clear;
+		}
+
+	}
+}
---
+++ shooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+	GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+		prefab = Resources.Load ("ball") as GameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			GameObject ball = Instantiate (prefab) as GameObject;
+			ball.transform.position = transform.position + ball.transform.forward * 2;
+			Rigidbody rb = ball.GetComponent<Rigidbody>();
+			rb.velocity = ball.transform.forward * 40;
+		}
+
+		}
+}
